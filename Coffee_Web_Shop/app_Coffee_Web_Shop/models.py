0de2976583f8ae478bdba7a8165bf207decc2f2b
MariaKilsved <40439902+MariaKilsved@users.noreply.github.com>
2023-11-22 14:05:54 +0100

    DB migration and superuser

--- Coffee_Web_Shop/app_Coffee_Web_Shop/models.py
+++ Coffee_Web_Shop/app_Coffee_Web_Shop/models.py
@@ -8,31 +8,31 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     nr_available = models.IntegerField()
     img = models.ImageField()
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self) -> str:
         return "({}) {}".format(self.product_id, self.name)
     
 class Customer(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
       
     def __str__(self) -> str:
         return self.user.username
     
 #Order table - contains order_ID(primary key), user_ID(foreign key-Order_Details table), order_date, total_price
-class Orders(models.Model):
+class Order(models.Model):
     order_id = models.AutoField(primary_key = True)
     user_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     order_date = models.DateTimeField()
-    total_price = models.DecimalField()
-
+    total_price = models.DecimalField(max_digits=7, decimal_places=2)
+    
     def __str__(self) -> str:
         return self.order_id
 
 ## This is class for Order-Details table. It connects to Orders, Product tables/classes
-class Order_Details(models.Model):
+class Order_Detail(models.Model):
     o_details_id = models.AutoField(primary_key=True)
-    order_id = models.ForeignKey(Orders,on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order,on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(blank=False)
 
