23e627bd44451f9a9c1a9eec9bd81e1bf285a015
MariaKilsved <40439902+MariaKilsved@users.noreply.github.com>
2023-11-22 11:38:53 +0100

    Product model

--- Coffee_Web_Shop/app_Coffee_Web_Shop/models.py
+++ Coffee_Web_Shop/app_Coffee_Web_Shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.Index()
+    name = models.CharField(max_length=255)
+    nr_available = models.IntegerField()
+    img = models.ImageField()
+    price = models.FloatField()
+
+    def __str__(self) -> str:
+        return "({}) {}".format(self.product_id, self.name)
