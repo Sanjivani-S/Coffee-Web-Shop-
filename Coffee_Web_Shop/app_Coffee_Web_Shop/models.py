78b5354f6e90a4c3957a3cf039629ff1c34d0453
GeerthiT <geerthi.t@outlook.com>
2023-11-22 11:39:56 +0100

    model.py Order table

--- Coffee_Web_Shop/app_Coffee_Web_Shop/models.py
+++ Coffee_Web_Shop/app_Coffee_Web_Shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    order_id = models.Index()
+    user_id = models.ForeignKey(Order_Details)
+    order_date = models.DateTimeField()
+    total_price = models.DecimalField()
+
+    def __str__(self) -> str:
+        return self.order_id
