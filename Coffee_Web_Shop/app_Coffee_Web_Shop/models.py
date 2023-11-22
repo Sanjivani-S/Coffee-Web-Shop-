c87cfb040efa9b819431e2433450d81fc62e583e
sanju soman <sanjivani.somanbapat@gmail.com>
2023-11-22 12:04:37 +0100

    changes to model.py. added Orders_details class

--- Coffee_Web_Shop/app_Coffee_Web_Shop/models.py
+++ Coffee_Web_Shop/app_Coffee_Web_Shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+## This is class for Order-Details table. It connects to Orders, Product tables/classes
+class Order_Details(models.Model):
+    o_details_id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(Orders,on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(blank=True)
+
+    def __str__(self) -> str :
+        return self.o_details_id
+
